75a4f236dd2ee6512fd40a640e2f3d6aca595f7e
Mengda (Albert) Yu <albertyu4913@gmail.com>
2018-11-21 00:03:42 -0800

    Add clean_data Rscript to read and save tidy data

---
+++ src/clean_data.R
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+
+##################################################
+## Project: clean_data.R
+## Date: 2o18 Nov 20 
+## Author: Mengda (Albert) Yu [aut cre] and Gilbert Lei [ctb]
+## Script purpose: This script reads in some raw data in csv format 
+##            and save a tidy version of it. It takes two arguments 
+##  
+## Example: Rscript clean_data.R data/data.csv data/data_tidy.csv
+##################################################
+
+# library
+suppressPackageStartupMessages(library(tidyverse))
+
+# define main function
+main <- function(){
+  
+  # read arguments from commend line
+  args <- commandArgs(trailingOnly = TRUE)
+  in_file <- args[1]
+  out_file <- args[2]
+  
+  # read in data
+  data <- suppressMessages(read_csv(in_file))
+  
+  # select feature 
+  out_data <- select_data(data)
+  
+  # save a tidy version of raw data
+  write_csv(out_data, out_file)
+}
+
+# select the data 
+select_data <- function(data){
+  
+  temp <- 
+    data %>% 
+    select(Gender, Purchase) %>% 
+    drop_na()
+  
+  return(temp)
+}
+
+# call main function
+main()
