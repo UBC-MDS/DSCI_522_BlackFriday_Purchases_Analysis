222423b0d83462428d5cbfc65a74943e8e6c6711
Mengda (Albert) Yu <albertyu4913@gmail.com>
2018-11-22 21:16:34 -0800

    Create generate_final_result.R, run_all.sh and organize Rscripts

---
+++ run_all.sh
@@ -0,0 +1,22 @@
+# test.sh
+
+###################################################
+## Date: 2018 Nov 22
+## Author: Mengda (Albert) Yu [aut cre] and Gilbert Lei [ctb]
+## Script purpose: NULL
+## Usage: bash run_all.sh
+##################################################
+
+# step 1
+Rscript src/01_clean_data.R data/BlackFriday.csv data/BlackFriday_tidy.csv
+
+# step 2 
+Rscript src/02_visualize_data.R data/BlackFriday_tidy.csv imgs
+
+# step 3
+Rscript src/03_analyze_data.R data/BlackFriday_tidy.csv results
+
+# step 4
+Rscript src/04_generate_final_result.R results imgs
+
+# step 5 make report
---
+++ src/04_generate_final_result.R
@@ -0,0 +1,91 @@
+#! /usr/bin/env python
+
+###################################################
+## Project: BF_purchase_analysis.R
+## Date: 2018 Nov 
+## Author: Mengda (Albert) Yu [aut cre] and Gilbert Lei [ctb]
+## Script purpose: This script is to read in the analysis result and generate 
+##              a summary figure and a hypothesis distribution. It take two arguments
+##              a path prefix pointing to the data and a path prefix where to save the figures
+## Example: Rscript src/generate_final_result.R results imgs
+##################################################
+
+# libraries
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(ggplot2))
+suppressPackageStartupMessages(library(scales))
+
+# define main function
+main <- function() {
+
+  # read arguments from commend line
+  args <- commandArgs(trailingOnly = TRUE)
+  in_file_prefix <- args[1]
+  out_file_prefix <- args[2]
+  
+  # in_file_prefix <- "results"
+  # out_file_prefix <- "imgs"
+  
+  # read in data
+  bf_est_path <- paste(in_file_prefix, "blackfriday_est.csv", sep = '/')
+  bf_est <- suppressWarnings(suppressMessages(read_csv(bf_est_path)))
+  t_test_result_path <- paste(in_file_prefix, "t_test_result.csv", sep = '/')
+  t_test_result <- suppressWarnings(suppressMessages(read_csv(t_test_result_path)))
+  
+  # create plot
+  errorbar <- plot_errorbar(bf_est)
+  tdist <- plot_tdist(t_test_result)
+  
+  # save the figures
+  ggsave("errorbar.png", plot = errorbar, path = out_file_prefix,
+         width = 8, height = 5)
+  ggsave("tdist.png", plot = tdist, path = out_file_prefix,
+         width = 8, height = 5)
+  
+  cat("Generating...Complete!\n")
+}
+
+# plot errorbar show the 95% confidence intervals and mean for each gender
+plot_errorbar <- function(bf_est) {
+  
+  errorbar <-
+    bf_est %>% 
+    ggplot() +
+    geom_point(aes(x = Gender, y = purchase_mean), color = "red") +
+    geom_errorbar(aes(x = Gender, ymin = lower_95, ymax = upper_95), 
+                  colour = "blue") +
+    scale_y_continuous(labels = dollar_format()) +
+    scale_x_discrete(labels = c("Female", "Male")) +
+    xlab("Gender") + 
+    ylab("The amount of purchase") +
+    ggtitle("The mean purchase with 95% confidence interval of female and male") +
+    theme_bw() +
+    theme(plot.title = element_text(size = 15, face = "bold", hjust = 0.5))
+    
+  return (errorbar)
+}
+
+# plot 
+plot_tdist <- function(t_test_result){
+  
+  tdist<- 
+    ggplot(data.frame(x = c(-4, 4)), aes(x = x)) +
+    stat_function(fun = dt, args = list(df = t_test_result$parameter), 
+                 lwd = 0.7) +
+    geom_vline(xintercept = t_test_result$statistic, color = "red", linetype = 'dashed') +
+    annotate("text", x = -40, y = 0.25, size = 3.5, 
+             label= paste("t-statistic\n", round(t_test_result$statistic, 2))) +
+    xlim(c(-47, 15)) +
+    ylab("density") +
+    xlab("") +
+    ggtitle("The T distribution under the null hypothesis H_0") +
+    theme_bw() +
+    theme(plot.title = element_text(size = 15, face = "bold", hjust = 0.5),
+          axis.title.y = element_text(face = "bold"))
+  
+   return (tdist)
+}
+
+# call main function
+main()
+
